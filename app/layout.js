399e1c3eee6d2a78ed9fa19215b317af737e5f7e
wheazzyfinesse <59867253+wheazzyfinesse@users.noreply.github.com>
2024-10-10 09:54:07 +0200

    done with ui

--- app/layout.js
+++ app/layout.js
@@ -23,8 +23,8 @@ export default function RootLayout({ children }) {
         className={inter.className}
       >
         <AuthProvider>
-          <Navbar />
           <LoadUser />
+          <Navbar />
           {children}
           <Toaster />
         </AuthProvider>
